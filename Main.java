31f878781c29342858ed9764bff25984782790df
utsav82 <utsa.yadav.94@gmail.com>
2023-09-07 01:14:18 +0530

    Update

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@
 
 import java.util.*;
 import java.io.*;
-import java.lang.reflect.Array;
+import java.lang.*;
 import java.math.BigInteger;
-import java.util.Map.Entry;
+
 
 /* Name of the class has to be "Main" only if the class is public. */
 public class Main {
@@ -25,32 +25,424 @@ public class Main {
         InputReader sc = new InputReader(inputStream);
         OutputWriter out = new OutputWriter(outputStream);
         Question solver = new Question();
+
         // precompFacts();
+
         int testCount = Integer.parseInt(sc.next());
         for (int i = 1; i <= testCount; i++) {
-
             // out.print("CASE #" + i + ": ");
             solver.solve(i, sc, out);
-
         }
         // solver.solve(1, sc, out);
         out.close();
     }
 
     static class Question {
+
         long dp[][];
 
-        public long gcd(long a, long b) {
-            if (b == 0)
-                return a;
-            return gcd(b, a % b);
-        }
-
         public void solve(int testNumber, InputReader sc, OutputWriter out) {
-
-           out.println("helddoo");
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int arr[] = sc.nextIntArray(n);
+            
 
         }
+
+    }
+
+    public static class Multiset<T> {
+        public TreeMap<T, Integer> map;
+        public int size = 0;
+
+        public Multiset() {
+            map = new TreeMap<>();
+        }
+
+        public Multiset(T[] a) {
+            map = new TreeMap<>();
+            size = a.length;
+            for (int i = 0; i < a.length; i++) {
+                map.put(a[i], map.getOrDefault(a[i], 0) + 1);
+            }
+        }
+
+        void add(T a) {
+            size++;
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+
+        void remove(T a) {
+            size--;
+            int val = map.get(a);
+            map.put(a, val - 1);
+            if (val == 1)
+                map.remove(a);
+        }
+
+        void removeAll(int a) {
+            if (map.containsKey(a)) {
+                size -= map.get(a);
+                map.remove(a);
+            }
+        }
+
+        T ceiling(T a) {
+            return map.ceilingKey(a);
+        }
+
+        T floor(T a) {
+            return map.floorKey(a);
+        }
+
+        T lower(T a) {
+            return map.lowerKey(a);
+        }
+
+        T higher(T a) {
+            return map.higherKey(a);
+        }
+
+        T first() {
+            return map.firstKey();
+        }
+
+        T last() {
+            return map.lastKey();
+        }
+
+        boolean contains(T a) {
+            return map.containsKey(a);
+        }
+
+        int size() {
+            return size;
+        }
+
+        void clear() {
+            map.clear();
+        }
+
+        T poll() {
+            if (map.size() == 0) {
+                return null;
+            }
+            size--;
+            T first = map.firstKey();
+            if (map.get(first) == 1) {
+                map.pollFirstEntry();
+            } else
+                map.put(first, map.get(first) - 1);
+            return first;
+        }
+
+        T polllast() {
+            if (map.size() == 0) {
+                return null;
+            }
+            size--;
+            T last = map.lastKey();
+            if (map.get(last) == 1) {
+                map.pollLastEntry();
+            } else
+                map.put(last, map.get(last) - 1);
+            return last;
+        }
+    }
+
+    static class Pair implements Comparable<Pair> {
+        long i;
+        int j;
+        long cost;
+
+        public Pair(long i, int j, long cost) {
+            this.i = i;
+            this.j = j;
+            this.cost = cost;
+
+        }
+
+        public Pair(long x, int y) {
+            this.i = x;
+            this.j = y;
+        }
+
+        public int compareTo(Pair o) {
+
+            int x = Long.compare(i, o.i);
+
+            return x;
+
+        }
+
+        // @Override
+        // public boolean equals(Object obj) {
+        // if (this == obj)
+        // return true;
+        // if (obj == null)
+        // return false;
+        // if (getClass() != obj.getClass())
+        // return false;
+        // Pair other = (Pair) obj;
+        // if (i != other.i)
+        // return false;
+        // if (j != other.j)
+        // return false;
+        // return true;
+        // }
+
+        // @Override
+        // public int hashCode() {
+        // final int prime = 31;
+        // int result = 1;
+        // result = prime * result + i;
+        // result = prime * result + j;
+        // return result;
+        // }
+
+    }
+
+    static long power(long x, long y, long p) {
+        long res = 1;
+
+        x = x % p;
+
+        if (x == 0)
+            return 0;
+
+        while (y > 0) {
+
+            if ((y & 1) != 0)
+                res = (res * x) % p;
+
+            y = y >> 1;
+            x = (x * x) % p;
+        }
+        return res;
+    }
+
+    static long modInverse(long b, long m) {
+
+        return (long) power(b, m - 2, m);
+
+    }
+
+    static int arraySortedOrNot(int arr[], int n) {
+
+        if (n == 1 || n == 0)
+            return 1;
+
+        if (arr[n - 1] >= arr[n - 2])
+            return 0;
+
+        return arraySortedOrNot(arr, n - 1);
+    }
+
+    public static <T extends Comparable<T>> T max(T a, T b) {
+        if (a.compareTo(b) >= 0) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static <T extends Comparable<T>> T min(T a, T b) {
+        if (a.compareTo(b) < 0) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    static class DisjointSet {
+        int[] s;
+
+        public DisjointSet(int n) {
+            Arrays.fill(s = new int[n], -1);
+        }
+
+        public int find(int i) {
+            return s[i] < 0 ? i : (s[i] = find(s[i]));
+        }
+
+        public boolean union(int a, int b) {
+            if ((a = find(a)) == (b = find(b)))
+                return false;
+            if (s[a] == s[b])
+                s[a]--;
+            if (s[a] <= s[b])
+                s[b] = a;
+            else
+                s[a] = b;
+            return true;
+        }
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    public long lcm(long a, long b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    static boolean isPrime(long n) {
+        for (long i = 2; i <= n / 2; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    static class SegmentTree {
+        long[] seg, lazy;
+
+        SegmentTree(int n) {
+            seg = new long[4 * n];
+            lazy = new long[4 * n];
+        }
+
+        void build(int low, int high, int ind, long[] arr) {
+            if (low == high) {
+                seg[ind] = arr[low];
+                return;
+            }
+
+            int mid = low + ((high - low) >> 1);
+
+            build(low, mid, 2 * ind + 1, arr);
+            build(mid + 1, high, 2 * ind + 2, arr);
+
+            seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+        }
+
+        void rangeUpdate(int l, int r, int low, int high, int ind, long val) {
+            if (lazy[ind] != 0) {
+                seg[ind] += (high - low + 1) * lazy[ind];
+                if (low != high) {
+                    lazy[2 * ind + 1] += lazy[ind];
+                    lazy[2 * ind + 2] += lazy[ind];
+                }
+                lazy[ind] = 0;
+            }
+
+            if (l > high || r < low)
+                return;
+
+            if (l <= low && high <= r) {
+                seg[ind] += (long) (high - low + 1) * val;
+
+                if (low != high) {
+                    lazy[2 * ind + 1] += val;
+                    lazy[2 * ind + 2] += val;
+                }
+                return;
+            }
+
+            int mid = low + ((high - low) >> 1);
+
+            rangeUpdate(l, r, low, mid, 2 * ind + 1, val);
+            rangeUpdate(l, r, mid + 1, high, 2 * ind + 2, val);
+
+            seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+        }
+
+        long query(int l, int r, int low, int high, int ind) {
+            if (lazy[ind] != 0) {
+                seg[ind] += (high - low + 1) * lazy[ind];
+
+                if (low != high) {
+                    lazy[2 * ind + 1] += lazy[ind];
+                    lazy[2 * ind + 2] += lazy[ind];
+                }
+                lazy[ind] = 0;
+            }
+
+            if (r < low || l > high)
+                return 0;
+
+            if (l <= low && high <= r) {
+                return seg[ind];
+            }
+
+            int mid = low + ((high - low) >> 1);
+            long left = query(l, r, low, mid, 2 * ind + 1);
+            long right = query(l, r, mid + 1, high, 2 * ind + 2);
+
+            return left + right;
+        }
+    }
+
+    static public ArrayList<Long> primeFactors(long n) {
+        ArrayList<Long> factorials = new ArrayList<>();
+        long limit = (long) Math.sqrt(n);
+        while (n % 2 == 0) {
+            factorials.add((long) 2);
+            n /= 2;
+        }
+        for (long i = 3; i <= limit; i += 2) {
+            while (n % i == 0) {
+                factorials.add(i);
+                n /= i;
+            }
+        }
+        if (n > 2)
+            factorials.add(n);
+        return factorials;
+    }
+
+    public int max(int[] elementData) {
+
+        int max = elementData[0];
+        for (int i = 1; i < elementData.length; i++) {
+            if (elementData[i] > max) {
+                max = elementData[i];
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public long max(long[] elementData) {
+
+        long max = elementData[0];
+        for (int i = 1; i < elementData.length; i++) {
+            if (elementData[i] > max) {
+                max = elementData[i];
+            }
+        }
+        return max;
+    }
+
+    public static long min(long[] array) {
+        long min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
     }
 
     static void precompFacts() {
@@ -118,7 +510,7 @@ public class Main {
         return (a % MOD - b % MOD + MOD) % MOD;
     }
 
-    public int binaryExponentiation(int x, int n) {
+    public static int binaryExponentiation(int x, int n) {
         if (n == 0)
             return 1;
         else if (n % 2 == 0)
@@ -127,7 +519,7 @@ public class Main {
             return x * binaryExponentiation(x * x, (n - 1) / 2);
     }
 
-    public long binaryExponentiation(long x, long n) {
+    public static long binaryExponentiation(long x, long n) {
         long result = 1;
         while (n > 0) {
             if (n % 2 == 1)
@@ -144,7 +536,8 @@ public class Main {
         else if (n % 2 == 0)
             return modularExponentiation(modularMultiplication(x, x, MOD), n / 2, MOD);
         else
-            return modularMultiplication(x, modularExponentiation(modularMultiplication(x, x, MOD), (n - 1) / 2, MOD),
+            return modularMultiplication(x,
+                    modularExponentiation(modularMultiplication(x, x, MOD), (n - 1) / 2, MOD),
                     MOD);
     }
 
@@ -467,6 +860,20 @@ public class Main {
         }
 
         public void println(int[] array) {
+            print(array);
+            writer.println();
+        }
+
+        public void print(long[] array) {
+            for (int i = 0; i < array.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(array[i]);
+            }
+        }
+
+        public void println(long[] array) {
             print(array);
             writer.println();
         }
--- hellp.cpp
+++ hellp.cpp
@@ -1,37 +1,39 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define int long long
-signed main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
 
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        if (n & 1)
-        {
-            cout << "YES" << endl;
-            continue;
-        }
-        int sum = 0;
-        for (int i = n - 1; i >= 0; i -= 2)
-        {
-            sum += (a[i] - a[i - 1]);
-        }
-        if (sum < 0)
-        {
-            cout << "NO" << endl;
-            continue;
-        }
-        cout << "YES" << endl;
-    }
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		int m;
+		cin >> n >> m;
+		int a[n];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a[i];
+		}
+		multiset<int> ele,diff;
+		ele.insert(0);
+		ele.insert(m);
+		diff.insert(m);
+		for(int i=0;i<n;i++){
+			ele.insert(a[i]);
+			auto it = ele.find(a[i]);
+			auto next = it;
+			auto prev = it;
+			next++;
+			prev--;
+			if(it!=ele.end() && it!=ele.begin()){
+				diff.erase(diff.find(*next-*prev));
+				diff.insert(*next-*it);
+				diff.insert(*it-*prev);
+			}
+			cout<<*diff.rbegin()<<" ";
+		}
+		cout<<endl;
+
+	}
 }
